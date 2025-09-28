9fee14b6bb47284467c35f798c3624afd89db4fd
milady-9 <alombeidah3@unemi.edu.ec>
2025-09-28 10:08:50 -0500

    updated

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
